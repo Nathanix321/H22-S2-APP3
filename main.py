330a9741e221d2bee74bc01fa58a55a73234daa1
Nathanix321 <letourneau.nathaniel@hotmail.com>
2022-02-10 12:02:01 -0500

    Voilà

--- main.py
+++ main.py
@@ -2,48 +2,40 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-TABMAX=38167
-ERRMAX=0.01
-RUNTIME=93842428.589/2
+TABMAX = 38167
+ERRMAX = 0.01
+RUNTIME = 93842428.589/2
 
-def coincidence(array1, array2, coincidence):
+def coincidence(array1, array2, coincidence, noncoincidence):
     j = 0
-    for i in range(0, TABMAX):
-        pos = array1[i][1] + ERRMAX
-        neg = array1[i][1] - ERRMAX
-        if neg > array2[i-j][1] or array2[i-j][1] > pos:
-            coincidence[i][2] = 0
-            continue
-        if array1[i][1] < array2[i - j][1]:
-            j += 1
-
-def noncoincidence(array1, array2, noncoincidence):
-    j = 0
-    for i in range(0, TABMAX):
-        pos = array1[i][1] + ERRMAX
-        neg = array1[i][1] - ERRMAX
-        if neg < array2[i-j][1] and array2[i-j][1] < pos:
+    i = 0
+    while i < TABMAX:
+        diff = array1[i][1]-array2[j][1]
+        if abs(diff) < 0.01:
+            coincidence.append(array1[i][2])
             noncoincidence[i][2] = 0
-            continue
-        if array1[i][1] < array2[i - j][1]:
             j += 1
+        if diff < 0:
+            i += 1
+            continue
+        if diff>0:
+            j += 1
+            i -= 1
+        i += 1
 
+def plot_loghist(x, bins, factor, histtype, colors, label):
 
-
-def plot_loghist(x, bins, factor):
   hist, bins = np.histogram(x, bins=bins)
   logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-  plt.hist(x, bins=logbins, weights=factor*np.ones_like(x))
+  plt.hist(x, bins=logbins, weights=factor*np.ones_like(x), histtype=histtype, edgecolor=colors, stacked=True, fill=False, label=label)
   plt.xscale('log')
 
 def tauxmuons(x):
-    t=RUNTIME/1000
-    tm=0
-    for i in range(TABMAX*2-1):
-        tm+=x[i][3]/1000
-    taux=((2*(TABMAX+1))/(t-tm))
-    print(tm)
-    print(taux)
+    t = RUNTIME/1000
+    tm = 0
+    for i in range(TABMAX-1):
+        tm += x[i][3]/1000
+    taux = 1/(t-tm)
     return taux
 
 def main():
@@ -54,19 +46,26 @@ def main():
     f1.close()
     f2.close()
 
-    coincid = np.copy(detect_prim)
-    coincidence(detect_prim, detect_sec, coincid)
-    mVc = coincid[:, 2]
+    mVc = []
+    mVn = np.copy(detect_prim)
+    coincidence(detect_prim, detect_sec, mVc, mVn)
+    mVc = np.array(mVc)
+    mVn = mVn[:, 2]
 
-    noncoincid = np.copy(detect_prim)
-    noncoincidence(detect_prim, detect_sec, noncoincid)
-    mVn = noncoincid[:, 2]
 
-    All = np.append(detect_prim, detect_sec, axis=0)
+    #noncoincidence(detect_prim, detect_sec, mVn)
+
+    test = np.append(mVc, mVn)
 
     plt.figure(num=1)
-    plt.title("All")
-    plot_loghist(All[:, 2], 10, tauxmuons(All))
+    n_bin=25
+    plt.title("Histogramme des distributions d'analyse de détection de muons")
+    plot_loghist(detect_prim[:, 2], n_bin, tauxmuons(detect_prim), 'step', 'grey', "Tous les évènements")
+    plot_loghist(mVc, n_bin, tauxmuons(detect_prim), 'step', 'red', "Évènements coincidents")
+    plot_loghist(mVn[mVn != 0], n_bin, tauxmuons(detect_prim), 'step', 'lime', "Évènments non-coincidents")
+    plt.xlabel("Voltage PMSi calculé [mV]")
+    plt.ylabel("Taux/classe [$s^-1$]")
+    plt.legend(loc="upper right")
     plt.show()
 
 
